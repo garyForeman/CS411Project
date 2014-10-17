28090d43c0b5c041020e4992493e5b209ca59b85
garyForeman <gforema2@illinois.edu>
2014-10-17 18:00:03 -0500

    Added an Update page with a single field that accepts a Sample ID.

--- FoxDB/app/views.py
+++ FoxDB/app/views.py
@@ -1,7 +1,16 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from .forms import UpdateForm
 
 @app.route('/')
 @app.route('/index')
 def index():
    return render_template('index.html', title='Home')
+
+@app.route('/update', methods=['GET', 'POST'])
+def update():
+    form = UpdateForm()
+    if form.validate_on_submit():
+        flash('Sampe ID = %s' % form.sample_id.data)
+        return redirect('/index')
+    return render_template('update.html', title='Update', form=form)

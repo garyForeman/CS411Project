f3f23fc557b062caaa7dab13e759208b2fe699a1
garyForeman <gforema2@illinois.edu>
2014-10-19 14:19:27 -0500

    Debugging

--- FoxDB/app/forms.py
+++ FoxDB/app/forms.py
@@ -7,9 +7,9 @@ class InsertForm(Form):
                             validators=[DataRequired(), Length(min=1, max=8)])
     name = StringField('name', validators=[Length(min=0, max=20)])
     generation = StringField('generation', validators=[Length(min=0, max=9)])
-    sex = RadioField('sex', 
-                     choices=[(1, 'Male'), (2, 'Female'), (None, 'Unkown')],
-                     default=None)
+    #sex = RadioField('sex', 
+    #                 choices=[(1, 'Male'), (2, 'Female'), (None, 'Unkown')],
+    #                 default=None)
     mother = StringField('mother', validators=[Length(min=0, max=8)])
     father = StringField('father', validators=[Length(min=0, max=8)])
     notes = TextAreaField('notes', validators=[Length(min=0, max=255)])

1c52e06b06185858cbfa025da33f22955c0e24a2
rando2 <rando2@illinois.edu>
2014-11-17 14:23:45 -0600

    New Error-Checker

---
+++ DBinit/Error_check_ed.py
@@ -0,0 +1,215 @@
+import MySQLdb
+import re
+
+def unfetchall_single(raw):
+    out = []
+    for x in raw:
+        out.append(','.join([str(i) for i in x]))
+    return out
+
+def unfetchall_multi(raw):
+    out = []
+    for x in raw:
+        out.append(','.join([str(i) for i in x]).split(","))
+    return out
+
+def unfetchone_multi(raw):
+    out = ""
+    for x in raw:
+        out+=(','.join(str(i) for i in x))
+    return out.split(",")
+
+def openconn():
+    conn = MySQLdb.connect (host = "localhost", user = "root",
+                            passwd = "foxdb@CS411", db = "foxdb")
+    c = conn.cursor ()
+    return c, conn
+
+def closeconn(conn):
+    conn.commit()
+    conn.close()
+
+def init(c):
+    c.execute("SHOW TABLES")
+    tables = c.fetchall()
+
+    alltables = []
+    for table in tables:
+        alltables.append('\t'.join([str(i) for i in table]))
+    print alltables
+    if "questionable" in alltables:
+        c.execute("DROP TABLE IF EXISTS questionable;")
+    c.execute("""CREATE TABLE IF NOT EXISTS questionable(cornellnumber VARCHAR(15), """+
+              """markername VARCHAR(15), checkedagainst VARCHAR(50), setname VARCHAR(50), ped VARCHAR(50), """+
+              """PRIMARY KEY(cornellnumber, markername, checkedagainst, setname, ped));""")
+
+    if "consistent_parents" in alltables:
+        c.execute("DROP TABLE IF EXISTS consistent_parents;")
+    c.execute("""CREATE TABLE consistent_parents(cornellnumber VARCHAR(15), markername VARCHAR(15),"""+
+              """setname VARCHAR(50), pedigree VARCHAR(50), PRIMARY KEY(cornellnumber, markername, setname, pedigree));""")
+
+    if "consistent_offspring" in alltables:
+        c.execute("DROP TABLE IF EXISTS consistent_offspring;")
+    c.execute("""CREATE TABLE consistent_offspring(cornellnumber VARCHAR(15), markername VARCHAR(15), """+
+              """setname VARCHAR(50), pedigree VARCHAR(50), OS_ID VARCHAR(15), PRIMARY KEY(cornellnumber, markername,setname, pedigree, OS_ID));""")
+
+    if "duds" in alltables:
+        c.execute("DROP TABLE IF EXISTS duds;")
+        
+    c.execute("""CREATE TABLE IF NOT EXISTS duds(cornellnumber VARCHAR(15),"""+
+              """markername VARCHAR(15),genotype1 INTEGER, genotype2 INTEGER,"""+
+              """PRIMARY KEY (cornellnumber, markername));""")
+
+def addtoquest(CN, marker, checkedagainst,setname, ped):
+    if CN!="NULL":
+        c.execute("""SELECT * FROM questionable WHERE cornellnumber='{0}' and markername='{1}' and setname='{2}'"""
+                  .format(CN,marker,setname))
+        if c.fetchone()==None:
+            c.execute("INSERT INTO questionable VALUES('{0}','{1}','{2}','{3}','{4}')"
+                      .format(CN, marker, checkedagainst, setname, ped))
+
+def checkgen(genotype_target, possible_inheritance, elders, target_CN, marker, setname, ped, genotypedict):
+    if '0' not in genotype_target:
+        if genotype_target[0] in possible_inheritance.keys() and genotype_target[1] in possible_inheritance.keys():#both gen_p in gen_gp
+            if genotype_target[0]==genotype_target[1] and len(possible_inheritance[genotype_target[0]])==2:#normal homozygote, both parents have it
+                c.execute("INSERT INTO consistent_parents VALUES('{0}','{1}','{2}','{3}')".format(target_CN, marker, setname, ped))
+                c.execute("INSERT INTO consistent_offspring VALUES('{0}','{1}','{2}','{3}','{4}')".format(elders[0], marker, setname, ped, target_CN))
+                c.execute("INSERT INTO consistent_offspring VALUES('{0}','{1}','{2}','{3}','{4}')".format(elders[1], marker, setname, ped, target_CN))
+                #normal heterozygote is next line
+            elif genotype_target[0] != genotype_target[1] and (genotype_target[0] in genotypedict[elders[0]] and genotype_target[1] in genotypedict[elders[1]]) or (genotype_target[1] in genotypedict[elders[0]] and genotype_target[0] in genotypedict[elders[1]]):
+                c.execute("INSERT INTO consistent_parents VALUES('{0}','{1}','{2}','{3}')".format(target_CN, marker, setname, ped))
+                c.execute("INSERT INTO consistent_offspring VALUES('{0}','{1}','{2}','{3}','{4}')".format(elders[0], marker, setname, ped, target_CN))
+                c.execute("INSERT INTO consistent_offspring VALUES('{0}','{1}','{2}','{3}','{4}')".format(elders[1], marker, setname, ped, target_CN))
+                
+        elif '0' in possible_inheritance.keys(): #if one or more parent does not have it
+            if len(possible_inheritance['0'])==1:
+                canskip = possible_inheritance['0'] #CN of the one that gets a pass
+                for elder in elders:
+                    if elder == canskip:
+                        addtoquest(elder, marker, 'offspring', setname, ped) #have no idea whether it's right or wrong
+                        continue
+                    else:
+                        if genotype_target[0] in genotypedict[elder]:#if the other one has this version
+                            c.execute("INSERT INTO consistent_parents VALUES('{0}','{1}','{2}','{3}')".format(target_CN, marker, setname, ped))
+                            c.execute("INSERT INTO consistent_offspring VALUES('{0}','{1}','{2}','{3}','{4}')".format(elder, marker, setname, ped, target_CN))
+            elif len(possible_inheritance['0'])==2:#both parents have a 0
+                addtoquest(target_CN, marker, 'parent', setname, ped)
+                addtoquest(elders[0], marker, 'offspring', setname, ped) #have no idea whether it's right or wrong
+                addtoquest(elders[1], marker, 'offspring', setname, ped) #have no idea whether it's right or wrong
+        else: #if parental alleles aren't in grandparental genotypes and it's not because of blanks
+            addtoquest(target_CN, marker, 'parent', setname, ped)#definitely wrong
+            addtoquest(elders[0], marker, 'offspring', setname, ped) #have no idea whether it's right or wrong
+            addtoquest(elders[1], marker, 'offspring', setname, ped) #have no idea whether it's right or wrong
+            
+    elif genotype_target == ['0','0']: #if both blanks
+        addtoquest(target_CN, marker, 'offspring', setname, ped) #have no idea whether it's right or wrong
+        addtoquest(target_CN, marker, 'parent', setname, ped) #have no idea whether it's right or wrong
+        
+    else: #if only one is '0'
+        realgen = 0
+        for genotype in target_genotype:
+            if genotype == '0':
+                continue
+            else:
+                realgen = genotype
+        if realgen in genotypedict[elders[0]]: 
+            c.execute("INSERT INTO consistent_parents VALUES('{0}','{1}','{2}','{3}')".format(target_CN, marker, setname, ped))
+            c.execute("INSERT INTO consistent_offspring VALUES('{0}','{1}','{2}','{3}','{4}')".format(elders[0], marker, setname, ped, target_CN))
+            addtoquest(elders[1], marker, 'offspring', setname, ped) #have no idea whether it's right or wrong
+        elif realgen in genotypedict[elders[1]]:
+            c.execute("INSERT INTO consistent_parents VALUES('{0}','{1}','{2}','{3}')".format(target_CN, marker, setname, ped))
+            c.execute("INSERT INTO consistent_offspring VALUES('{0}','{1}','{2}','{3}','{4}')".format(elders[1], marker, setname, ped, target_CN))
+            addtoquest(elders[0], marker, 'offspring', setname, ped) #have no idea whether it's right or wrong
+        else:
+            addtoquest(target_CN, marker, 'parent', setname, ped) #have no idea whether it's right or wrong
+            addtoquest(elders[0], marker, 'offspring', setname, ped) #have no idea whether it's right or wrong
+            addtoquest(elders[1], marker, 'offspring', setname, ped) #have no idea whether it's right or wrong
+
+
+            
+[c,conn]=openconn()
+init(c)
+c.execute("SELECT DISTINCT markername FROM has_genotype")
+allmarkers = unfetchall_single(c.fetchall())
+
+c.execute("SHOW TABLES LIKE 'set%';")
+allsets = unfetchall_single(c.fetchall())
+
+for setname in allsets: #set by set
+    c.execute("SELECT DISTINCT pedigree FROM {0}".format(setname))
+    pedigrees = unfetchall_single(c.fetchall())
+
+    for ped in pedigrees: #pedigree by pedigree
+
+        c.execute("SELECT cornellnumber FROM {0} WHERE pedigree='{1}' and generation=2"
+                  .format(setname, ped))
+        samples_II = unfetchall_single(c.fetchall())
+
+        c.execute("SELECT cornellnumber FROM {0} WHERE pedigree='{1}' and generation=3"
+                  .format(setname, ped))
+        samples_III = unfetchall_single(c.fetchall())
+        
+        for marker in allmarkers:
+            I_genotypes = {}
+            II_genotypes = {}
+            
+            possible_inheritance_p={}
+            for CN_p in samples_II:
+                c.execute("""SELECT genotype1, genotype2 FROM has_genotype WHERE """+
+                          """cornellnumber ='{0}' AND markername='{1}';"""
+                          .format(CN_p, marker))
+                genotype_p = unfetchone_multi(c.fetchall())
+                II_genotypes[CN_p]=genotype_p
+                
+                if genotype_p==['']: #this will happen for 'ped37dam' and 'ped8sire'
+                    c.execute("INSERT INTO questionable VALUES('{0}','{1}','parent','{2}','{3}')"
+                              .format(CN_p, marker, setname, ped))#definitely wrong
+                    continue
+
+                for gen in genotype_p: #for each of the grandparents' alleles, record which gp(s) contributed allele
+                    if gen not in possible_inheritance_p.keys():
+                        possible_inheritance_p[gen] = [CN_p]
+                    elif possible_inheritance_p[gen]!= [CN_p]:
+                        possible_inheritance_p[gen].append(CN_p)
+                
+                possible_inheritance_gp= {}
+                c.execute("SELECT mother, father FROM sample_info_clean WHERE cornellnumber='{0}'"
+                          .format(CN_p))
+                grandparents = unfetchone_multi(c.fetchall())
+
+                for CN_gp in grandparents:
+                    if CN_gp != "NULL":
+                        c.execute("""SELECT genotype1, genotype2 FROM has_genotype WHERE """+
+                                  """cornellnumber ='{0}' AND markername='{1}';"""
+                                  .format(CN_gp, marker))
+                        gp_genotype = unfetchone_multi(c.fetchall())
+                    else:
+                        gp_genotype = ['0','0']
+                    I_genotypes[CN_gp]=gp_genotype
+
+                    for gen in gp_genotype: #for each of the grandparents' alleles, record which gp(s) contributed allele
+                        if gen not in possible_inheritance_gp.keys():
+                            possible_inheritance_gp[gen] = [CN_gp]
+                        elif possible_inheritance_gp[gen]!= [CN_gp]:
+                            possible_inheritance_gp[gen].append(CN_gp)
+                            
+                checkgen(genotype_p, possible_inheritance_gp, grandparents, CN_p, marker, setname, ped, I_genotypes)
+                
+            for CN_kit in samples_III:
+                c.execute("""SELECT genotype1, genotype2 FROM has_genotype WHERE """+
+                          """cornellnumber ='{0}' AND markername='{1}';""".format(CN_p, marker))
+                genotype_k = unfetchone_multi(c.fetchall())
+
+                if genotype_k==['']: #this will happen for 'ped37dam' and 'ped8sire'
+                    c.execute("INSERT INTO questionable VALUES('{0}','{1}','parent','{2}','{3}')"
+                              .format(CN_kit, marker, setname, ped))#definitely wrong
+                    continue
+
+                checkgen(genotype_k, possible_inheritance_p, samples_II, CN_kit, marker, setname, ped, II_genotypes)
+                
+                        
+                        
+closeconn(conn)
+                
+      
+

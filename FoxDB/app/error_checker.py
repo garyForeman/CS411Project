b954712ac8873aee014e9ff8bca766ecb87670ff
garyForeman <gforema2@illinois.edu>
2014-11-29 16:32:05 -0600

    Added Halie's error checker update to web app.

--- FoxDB/app/error_checker.py
+++ FoxDB/app/error_checker.py
@@ -223,24 +223,79 @@ def checkgen(genotype_target, possible_inheritance, elders, target_CN,
             #have no idea whether it's right or wrong
             addtoquest(elders[1], marker, 'offspring', setname, ped, c)
 
-    elif genotype_target == ['0', '0']: #if both blanks
+    else: #if it includes blanks
         #have no idea whether it's right or wrong
         addtoquest(target_CN, marker, 'offspring', setname, ped, c)
         #have no idea whether it's right or wrong
         addtoquest(target_CN, marker, 'parent', setname, ped, c)
-        #have no idea whether it's right or wrong
-        addtoquest(elders[0], marker, 'offspring', setname, ped, c)
-        #have no idea whether it's right or wrong
-        addtoquest(elders[1], marker, 'offspring', setname, ped, c)
-    #if exactly one allele of target_genotype is '0' -> Jen wants to
-    #hand-validate these
-    else:
-        #have no idea whether it's right or wrong
-        addtoquest(target_CN, marker, 'parent', setname, ped, c)
-        #have no idea whether it's right or wrong
-        addtoquest(elders[0], marker, 'offspring', setname, ped, c)
-        #have no idea whether it's right or wrong
-        addtoquest(elders[1], marker, 'offspring', setname, ped, c)
+
+        c.execute("""SELECT has_genotype.cornellnumber, genotype1, """ +
+                  """genotype2 FROM sample_info_clean, has_genotype """ +
+                  """WHERE sample_info_clean.cornellnumber=""" +
+                  """has_genotype.cornellnumber AND markername ='""" +
+                  marker + """' AND (mother = '""" + elders[0] +
+                  """' AND father = '""" + elders[1] +
+                  """') OR (father = '""" + elders[0] + """' and mother = '""" +
+                  elders[1] + """');""")
+        sib_genotypes = unfetchall_multi(c.fetchall())
+        ever_error = 0
+        for sibgen in sib_genotypes:
+            if ever_error == 1: #once we've found a mistake, don't care anymore
+                continue
+
+            sib_CN = sibgen[0]
+            if sib_CN == target_CN:
+                continue
+
+            genotype_sib = sibgen[1:3]
+            if '0' in genotype_sib:
+                continue
+            elif (genotype_sib[0] in possible_inheritance.keys() and
+                  genotype_sib[1] in possible_inheritance.keys()):
+                #both alleles found in parents
+                if genotype_sib[0] == genotype_sib[1]: #if homozygotic
+                    if len(possible_inheritance[genotype_sib[0]]) < 2:
+                        #normal homozygote, both parents have it
+                        #have no idea whether it's right or wrong
+                        addtoquest(elders[0], marker, 'offspring', setname,
+                                   ped, c)
+                        #have no idea whether it's right or wrong
+                        addtoquest(elders[1], marker, 'offspring', setname,
+                                   ped, c)
+                        ever_error = 1
+                elif genotype_target[0] != genotype_target[1]:
+                    #if heterozygote THIS IS THE TROUBLE CLAUSE
+                    if (genotype_target[0] in genotypedict[elders[0]] and
+                        genotype_target[1] in genotypedict[elders[1]]):
+                        continue #it's good, so move along
+                    elif (genotype_target[1] in genotypedict[elders[0]] and
+                          genotype_target[0] in genotypedict[elders[1]]):
+                        continue #it's good, so move along
+                    else:
+                        addtoquest(elders[1], marker, 'offspring', setname,
+                                   ped, c)
+                        addtoquest(elders[0], marker, 'offspring', setname,
+                                   ped, c)
+                        ever_error = 1
+                else: #this doesn't happen. SCORE!
+                    addtoquest(elders[1], marker, 'offspring', setname, ped, c)
+                    addtoquest(elders[0], marker, 'offspring', setname, ped, c)
+                    ever_error = 1
+            else:
+                #if parental alleles aren't in grandparental genotypes and
+                #it's not because of blanks
+                #have no idea whether it's right or wrong
+                addtoquest(elders[0], marker, 'offspring', setname, ped, c)
+                #have no idea whether it's right or wrong
+                addtoquest(elders[1], marker, 'offspring', setname, ped, c)
+                ever_error = 1
+        if ever_error == 0:
+            c.execute("INSERT INTO consistent_offspring " +
+                      "VALUES('{0}','{1}','{2}','{3}','{4}')"
+                      .format(elders[0], marker, setname, ped, target_CN))
+            c.execute("INSERT INTO consistent_offspring " +
+                      "VALUES('{0}','{1}','{2}','{3}','{4}')"
+                      .format(elders[1], marker, setname, ped, target_CN))
 
 def error_checker(c):
     init(c)
